997c1d56c00fc73281010ccda7fb9fe979e5d2b9
Helen Anderson <helen.anderson26@gmail.com>
2017-04-09 15:54:53 +0100

    beautified

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
+    // When eyes button is clicked, toggle laser class on brain
+    $(".flash").click(function() {
+        $(".brain").toggleClass('laser');
+    });
 
-// When eyes button is clicked, toggle laser class on brain
-$(".flash").click(function() {
-  $(".brain").toggleClass('laser');
+    // When color button is clicked...
+    $(".color").click(function() {
+        // assign each named color a random number 0-255
+        var red = Math.floor(Math.random() * 255);
+        var green = Math.floor(Math.random() * 255);
+        var blue = Math.floor(Math.random() * 255);
+
+        // Generate an RGBA value from red green and blue
+        var randomRGBA = 'rgba(' + red + ',' + green + ',' + blue + ',1)';
+        $("body").css("background", randomRGBA);
+        //Display the RGBA values in an alert window
+        //alert(randomRGBA);
+    });
+
 });
-
-// When color button is clicked...
-$(".color").click(function() {
-  // assign each named color a random number 0-255
-  var red = Math.floor(Math.random() * 255);
-  var green = Math.floor(Math.random() * 255);
-  var blue = Math.floor(Math.random() * 255); 
-
-  // Generate an RGBA value from red green and blue
-  var randomRGBA = 'rgba('+red+','+green+','+blue+',1)';
-  $("body").css("background", randomRGBA);
-  //Display the RGBA values in an alert window
-  //alert(randomRGBA);
-});
-
-});
-
-
